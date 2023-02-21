e076e99d1639c17be04768ed438935e0dcb1ccfa
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-02-22 01:13:48 +0300

    refactor: update app config for frontend app

--- backend/src/main/java/com/capgemini/config/AppConfig.java
+++ backend/src/main/java/com/capgemini/config/AppConfig.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.time.format.DateTimeFormatter;
 
@@ -12,10 +14,11 @@ import java.time.format.DateTimeFormatter;
  * Configuration file for the application
  */
 @Configuration
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
 
     private static final String DATE_FORMAT = "dd.MM.yyyy";
     private static final String DATE_TIME_FORMAT = "dd.MM.yyyy HH:mm:ss";
+    private static final String ALLOWED_ORIGINS = "http://localhost:3000";
 
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
@@ -24,5 +27,12 @@ public class AppConfig {
             builder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern(DATE_FORMAT)));
             builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATE_TIME_FORMAT)));
         };
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(ALLOWED_ORIGINS)
+                .allowedMethods("GET", "POST");
     }
 }
