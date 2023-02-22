1c31f727f512beb471f9d1bd58984e49e0aeec68
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-02-22 11:30:57 +0300

    refactor: update query

--- backend/src/main/java/com/capgemini/repository/TransactionRepository.java
+++ backend/src/main/java/com/capgemini/repository/TransactionRepository.java
@@ -10,8 +10,8 @@ import java.util.List;
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
     @Query(value = "SELECT t " +
-            "FROM Account a " +
-            "LEFT JOIN Transaction t ON a.id = t.account.id " +
+            "FROM Transaction t " +
+            "LEFT JOIN Account a ON t.account.id = a.id " +
             "WHERE a.customer.id = :customerId " +
             "ORDER BY t.date DESC")
     List<Transaction> findAllByCustomerId(@Param("customerId") Long customerId);
